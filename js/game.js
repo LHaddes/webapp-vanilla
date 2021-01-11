dd87b4659146de7d0ed709d122c27c8a9232503c
Kilian <haddesgaming@gmail.com>
2021-01-11 10:58:34 +0100

    écran veille windows

--- js/game.js
+++ js/game.js
@@ -1,1 +1,45 @@
-console.log("Hello!")
+const canvas = document.getElementById("canvasID");
+const ctx = canvas.getContext("2d");
+
+let x = 10;
+let y = 10;
+let boolX = false;
+let boolY = false;
+
+function GameLoop(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(x, y, 100, 100);
+    
+    if(x + 100 >= canvas.width || x <= 0)
+    {
+        boolX = !boolX;
+    }
+    if(y + 100 >= canvas.height || y <= 0)
+    {
+        boolY = !boolY;
+    }
+
+    if(boolX == false)
+    {
+        x += 2;
+    }
+    else
+    {
+        x -= 2;
+    }
+
+    if(boolY == false)
+    {
+        y += 1.5;
+    }
+    else
+    {
+        y -= 1.5;
+    }
+}
+
+
+setInterval(GameLoop, 1000 / 60);
