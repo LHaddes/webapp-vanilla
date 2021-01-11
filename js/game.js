844f8325f4e8bd8c7a09ea213871084f3f9146d0
Kilian <haddesgaming@gmail.com>
2021-01-11 12:19:34 +0100

    Optimisation avec listes et prend toute la taille de l'écran

--- js/game.js
+++ js/game.js
@@ -1,30 +1,72 @@
 const canvas = document.getElementById("canvasID");
 const ctx = canvas.getContext("2d");
 
-let x = 10;
-let y = 10;
-let xMove = 2;
-let yMove = 2;
+
+canvas.width = document.documentElement.clientWidth || document.body.clientWidth;
+canvas.height = document.documentElement.clientHeight || document.body.clientHeight;
+
+function RectCreate(x, y, w, h, color, dw, dy)
+{
+    let obj = {
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        color: color,
+        dw: dw,
+        dy: dy,
+        draw: RectDraw
+    }
+    return obj
+}
+
+let rect = RectCreate(10, 10, 100, 100, "red", 2, 2)
+let rect2 = RectCreate(200, 200, 70, 70, "green", 4, 4)
+
+let listRect = [
+    rect,
+    rect2
+]
+
 
 
 function GameLoop(){
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(x, y, 100, 100);
-    
-    if(x + 100 >= canvas.width || x <= 0)
+    //rect.draw();
+    //rect2.draw();
+
+    /*for(let i = 0; i < listRect.length; i++)    //for
     {
-        xMove *= -1;
-    }
-    if(y + 100 >= canvas.height || y <= 0)
+        listRect[i].draw;
+    }*/
+
+    for(const obj of listRect)  //foreach
     {
-        yMove *= -1;
+        obj.draw()
     }
 
-    x += xMove;
-    y += yMove;
+    /*listRect.forEach((obj) => {obj.draw() }) //foreach*/
+    
+}
+
+function RectDraw()
+{
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+
+    if(this.x + this.w >= canvas.width || this.x <= 0)
+    {
+        this.dw *= -1;
+    }
+    if(this.y + this.h >= canvas.height || this.y <= 0)
+    {
+        this.dy *= -1;
+    }
+
+    this.x += this.dw;
+    this.y += this.dy;
 }
 
 
