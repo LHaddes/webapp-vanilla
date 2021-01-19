4bc89852cad2db94da068bf00f04291eab85e93e
Kilian <haddesgaming@gmail.com>
2021-01-19 13:29:54 +0100

    TouchEvent start

--- js/game.js
+++ js/game.js
@@ -5,6 +5,32 @@ const ctx = canvas.getContext("2d");
 canvas.width = document.documentElement.clientWidth || document.body.clientWidth;
 canvas.height = document.documentElement.clientHeight || document.body.clientHeight;
 
+function startup() {
+    canvas.addEventListener("touchstart", handleStart, false);
+    canvas.addEventListener("touchend", handleEnd, false);
+    canvas.addEventListener("touchcancel", handleCancel, false);
+    canvas.addEventListener("touchmove", handleMove, false);
+  }
+
+
+function handleStart(evt) {
+    evt.preventDefault();
+    console.log("touchstart.");
+    
+    var touches = evt.changedTouches;
+  
+    for (var i = 0; i < touches.length; i++) {
+      console.log("touchstart:" + i + "...");
+      ongoingTouches.push(copyTouch(touches[i]));
+      var color = colorForTouch(touches[i]);
+      ctx.beginPath();
+      ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
+      ctx.fillStyle = color;
+      ctx.fill();
+      console.log("touchstart:" + i + ".");
+    }
+}
+
 function RectCreate(x, y, w, h, color, dw, dy)
 {
     let obj = {
